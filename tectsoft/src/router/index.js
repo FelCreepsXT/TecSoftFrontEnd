3fed3f8225b31e57ec7d7b7226abe0b222470c97
LucianoLopez <55890030+FelCreepsXT@users.noreply.github.com>
2020-11-19 17:19:07 -0500

    First commit

--- tectsoft/src/router/index.js
+++ tectsoft/src/router/index.js
@@ -10,10 +10,50 @@ const routes = [
     name: 'Home',
     component: Home
   },
+            //Dispatcher
   {
-    path: '/about',
-    name: 'About',
-    component: () => import('../views/About.vue')
+   path: '/loginDispatcher',
+   name: 'LoginDispatcher',
+    component: () => import('../views/Dispatcher/loginDispatcher')
+  },
+  {
+    path: '/dispatcher/:dispatcherId',
+    name: 'AppDispatcher',
+    component: () => import('../views/Dispatcher/appDispatcher'),
+    children: [
+      {
+        path: 'home',
+        name: 'HomeDispatcher',
+        component: () => import('../views/Dispatcher/homeDispatcher')
+      },
+      {
+        path: 'package',
+        name: 'PackageDispatcher',
+        component: () => import('../components/Dispatcher/updatePackageDispatcher'),
+        children: [
+          {
+            path: '0',
+            name: 'DispatcherState0',
+            component: () => import('../components/Dispatcher/StatePackage/PackageNull')
+          },
+          {
+            path: '1',
+            name: 'DispatcherState1',
+            component: () => import('../components/Dispatcher/StatePackage/PackageState1')
+          },
+          {
+            path: '2',
+            name: 'DispatcherState2',
+            component: () => import('../components/Dispatcher/StatePackage/PackageState2')
+          },
+          {
+            path: '3',
+            name: 'DispatcherState3',
+            component: () => import('../components/Dispatcher/StatePackage/PackageState3')
+          }
+        ]
+      }
+    ]
   }
 ]
 
---
+++ tectsoft/src/views/Dispatcher/appDispatcher.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app>
+    <v-app-bar app color="#00C2FF">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>
+        <h1 style="color: white">Virtual Express</h1>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn @click="navigateToHome">
+          Home
+        </v-btn>
+        <v-btn @click="navigateToPackage">
+          Package
+        </v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+    <v-footer app>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "AppDispatcher",
+  methods: {
+    navigateToHome(){
+      this.$router.push({name: 'HomeDispatcher', params: {dispatcherId: this.$route.params.dispatcherId}}).catch(() => {});
+    },
+    navigateToPackage(){
+      this.$router.push({name: 'DispatcherState0', params: {dispatcherId: this.$route.params.dispatcherId}}).catch(() => {});
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

63a2ae398677b1e07a994dcd581e359564d1c063
Enzo <43630731+EnzoHuacacolque@users.noreply.github.com>
2020-11-25 22:25:09 -0500

    Upload

---
+++ tectsoft/src/views/Company/appCompany.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app>
+    <v-app-bar app color="#00C2FF">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>
+        <h1 style="color: white">Virtual Express</h1>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn @click="navigateToHome">
+          Data's
+        </v-btn>
+        <v-btn @click="navigateToShipTerminalByCompanyId">
+          Terminals
+        </v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+    <v-footer app>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "AppCompany",
+  methods: {
+    navigateToHome(){
+      this.$router.push({name: 'HomeCompany', params: {companyId: this.$route.params.companyId}}).catch(() => {});
+    },
+    navigateToShipTerminalByCompanyId(id){
+      this.$router.push({name: 'ShipTerminal',params:{companyId: this.$route.params.companyId}}).catch(() => {});
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ tectsoft/src/views/Company/homeCompany.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-card>
+    <h1>Bienvenido {{ company.name }}</h1>
+    <v-list-item-avatar
+        tile
+        size="80"
+        color="grey"
+    ></v-list-item-avatar>
+    <v-list-item-subtitle>
+      {{ company.description }}
+    </v-list-item-subtitle>
+  </v-card>
+</template>
+
+<script>
+import CompanyService from '@/services/company/company-service'
+
+export default {
+  name: "HomeCompany",
+  data(){
+    return{
+      company: []
+    }
+  },
+  methods: {
+    retrieveCompany(id){
+      CompanyService.getId(id)
+          .then(response => {
+            this.company = response.data;
+            console.log(this.company);
+          })
+          .catch(e => {
+            console.log(e);
+          })
+    }
+  },
+  mounted() {
+    this.retrieveCompany(this.$route.params.companyId);
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
